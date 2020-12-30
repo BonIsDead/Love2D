246e4555c78f98cf288cf9674cd4c6e87975767c
BonIsDead <xbonofthedeadx@gmail.com>
2020-12-29 20:12:09 -0500

    Working on LDtk importing

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 require "assets/entities/player"
+json = require "lib/json"
 
 -- Global variables
 _gameWidth = love.graphics.getWidth()
@@ -8,8 +9,48 @@ _gameHeight = love.graphics.getHeight()
 _entities = {}
 player = Player:new(32, 32)
 
-tex = love.graphics.newImage("assets/spritesheets/FinalFantasy6Tileset.png")
-tile = love.graphics.newQuad(16,16, 16,16, tex)
+-- -------------------- Tiles
+path = love.filesystem.read("assets/levels/test.ldtk")
+mapData = json.decode(path)
+
+-- Populate layers
+local layerData = mapData.defs.layers
+local layers = {}
+
+for v, i in ipairs(layerData) do
+    i.identifier = layerData[v].identifier
+    i.type = layerData[v].type
+    i.uid = layerData[v].uid
+    i.gridSize = layerData[v].gridSize
+    i.displayOpacity = layerData[v].displayOpacity
+    table.insert(layers, i)
+end
+
+local levelData = mapData.defs.levels
+local levels = {}
+
+-- TODO LIST
+-- [ ] Dump data into tables
+-- [ ] Read data from tables
+-- [ ] Visualize table data
+
+-- for v, i in ipairs(levelData) do
+--     local ld = levelData[v]
+--     i.identifier = ld.identifier
+--     i.uid = ld.uid
+--     i.worldX, i.worldY = ld.worldX, ld.worldY
+--     i.pxWid, i.pxHei = ld.pxWid, ld.pxHei
+--     i.bgColor = ld.bgColor
+
+--     i.layerInstances = {}
+    
+--     for v, i in ipairs(i.layerInstances.gridTiles) do
+--         table.insert(i.layerInstances, i.layerInstances.gridTiles[v] )
+--     end
+-- end
+
+-- tex = love.graphics.newImage("assets/spritesheets/FinalFantasy6Tileset.png")
+-- tile = love.graphics.newQuad(16,16, 16,16, tex)
 
 function love.load()
     -- Load entities
@@ -26,11 +67,11 @@ function love.update(dt)
 end
 
 function love.draw()
-    for x=0,15 do
-        for y=0,13 do
-            love.graphics.draw(tex, tile, x*16,y*16)
-        end
-    end
+    -- for x=0,15 do
+    --     for y=0,13 do
+    --         love.graphics.draw(tex, tile, x*16,y*16)
+    --     end
+    -- end
 
     -- Draw entities
     for i, ent in ipairs(_entities) do
