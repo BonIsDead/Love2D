a90bf9bdcd5e1c031178885faed6262f350119e7
BonIsDead <xbonofthedeadx@gmail.com>
2020-12-30 00:50:41 -0500

    I broke it...

--- main.lua
+++ main.lua
@@ -13,10 +13,11 @@ player = Player:new(32, 32)
 path = love.filesystem.read("assets/levels/test.ldtk")
 mapData = json.decode(path)
 
--- Populate layers
+-- ---------- Populate layers
 local layerData = mapData.defs.layers
 local layers = {}
 
+-- Fill the layers table
 for v, i in ipairs(layerData) do
     i.identifier = layerData[v].identifier
     i.type = layerData[v].type
@@ -26,33 +27,54 @@ for v, i in ipairs(layerData) do
     table.insert(layers, i)
 end
 
-local levelData = mapData.defs.levels
+-- ---------- Populate levels
+local levelData = mapData.levels
 local levels = {}
 
--- TODO LIST
--- [ ] Dump data into tables
--- [ ] Read data from tables
--- [ ] Visualize table data
+-- Fill the levels table
+for i=1,#levelData do
+    local ld = levelData[i]
+    local l = {}
 
--- for v, i in ipairs(levelData) do
---     local ld = levelData[v]
---     i.identifier = ld.identifier
---     i.uid = ld.uid
---     i.worldX, i.worldY = ld.worldX, ld.worldY
---     i.pxWid, i.pxHei = ld.pxWid, ld.pxHei
---     i.bgColor = ld.bgColor
+    -- Get level data
+    l.identifier, l.uid = ld.__identifier, ld.uid
+    l.worldX, l.worldY = ld.worldX, ld.worldY
+    l.pxWid, l.pxHei = ld.pxWid, ld.pxHei
+    l.bgColor = ld.__bgColor
 
---     i.layerInstances = {}
-    
---     for v, i in ipairs(i.layerInstances.gridTiles) do
---         table.insert(i.layerInstances, i.layerInstances.gridTiles[v] )
---     end
--- end
+    -- Get layer instances
+    l.layerInstances = {}
+    for i=1,#ld.layerInstances do
+        l.layerInstances[i] = ld.layerInstances[i]
+    end
 
+    -- Add to the levels table
+    table.insert(levels, l)
+end
+
+-- It can read the files! Hurray!
+print(levels[1].layerInstances[1].gridTiles[1].px[1] )
+
+-- tilesetPath = levels[1].layerInstances[1].__tilesetRelPath
+-- tex = love.graphics.newImage(tilesetPath)
+
+-- Just here so I don't forget how to do this!!!
 -- tex = love.graphics.newImage("assets/spritesheets/FinalFantasy6Tileset.png")
 -- tile = love.graphics.newQuad(16,16, 16,16, tex)
 
+-- package.path = 
+
 function love.load()
+    print("PP - " .. package.path)
+    print("Love Source - " .. love.filesystem.getSource() )
+    tilesetPath = "../spritesheets/FinalFantasy6Tileset.png" --levels[1].layerInstances[1].__tilesetRelPath
+    print(tilesetPath)
+    if love.filesystem.getInfo(tilesetPath) then
+        print("It worked!")
+    else
+        print("File not found!")
+    end
+
     -- Load entities
     for i, ent in ipairs(_entities) do
         if ent.start then ent:start() end
